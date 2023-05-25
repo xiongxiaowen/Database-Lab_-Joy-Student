e8df343ceb61e88a875c5fbd5e40672d88036881
xiong <xiongxiaowen0203@gmail.com>
2023-05-25 19:26:01 +0300

    25th May

---
+++ venv/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from os import getenv
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+
+import routes
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ venv/routes.py
@@ -0,0 +1,34 @@
+from app import app
+from flask import render_template, request
+import users
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html")
