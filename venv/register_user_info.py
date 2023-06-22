c59815e4814bb85b0f837e86e808ca8f6d06c867
xiong <xiongxiaowen0203@gmail.com>
2023-06-22 22:36:23 +0300

    save user info to table

--- venv/register_user_info.py
+++ venv/register_user_info.py
@@ -3,13 +3,18 @@ import users
 from sqlalchemy import text
 
 
-def save_user_info(user_id, name, gender, faculty, student_number, address):
+def save_user_info(user_id, name):
     try:
-        sql = text("INSERT INTO usersinfo (user_id, users_name, gender, faculty, student_number, address_user) "
-                   "VALUES (:user_id, :name, :gender, :faculty, :student_number, :address)")
-        db.session.execute(sql, {"user_id": user_id, "name": name, "gender": gender, "faculty": faculty,
-                                 "student_number": student_number, "address": address})
+        sql = text("INSERT INTO usersinfo (user_id, users_name) "
+                   "VALUES (:user_id, :name)")
+        db.session.execute(sql, {"user_id": user_id, "name": name})
         db.session.commit()
     except:
         return False
     return True
+
+def get_user_by_id(user_id):
+    sql = text("SELECT * FROM usersinfo WHERE user_id = :user_id")
+    result = db.session.execute(sql, {"user_id": user_id})
+    user_info = result.fetchone()
+    return user_info
