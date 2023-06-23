a7a9d2170c1c8c6b4422e1ae27883ef0000e706b
xiong <xiongxiaowen0203@gmail.com>
2023-06-23 18:41:42 +0300

    add friend function

---
+++ venv/friend.py
@@ -0,0 +1,26 @@
+from db import db
+import users
+from sqlalchemy import text
+
+def add_friend(user_id, friend_id):
+    # Check if the friendship already exists
+    sql = text("SELECT user_id FROM friends WHERE user_id = :user_id AND friend_id = :friend_id")
+    result = db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id})
+    existing_friendship = result.fetchone()
+
+    if existing_friendship:
+        # Friendship already exists, no need to add again
+        return False
+    else:
+        # Add the friendship
+        sql = text("INSERT INTO friends (user_id, friend_id) VALUES (:user_id, :friend_id)")
+        db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id})
+        db.session.commit()
+        return True
+
+def get_friends(user_id):
+    sql = text("SELECT usersinfo.user_id, usersinfo.users_name FROM usersinfo JOIN friends ON usersinfo.id = friends.friend_id WHERE friends.user_id = :user_id")
+    result = db.session.execute(sql, {"user_id": user_id})
+    friends = result.fetchall()
+    return friends
+
